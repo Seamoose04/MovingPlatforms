7151b0f478e5c0b0f8ba6783a6051423d77773b0
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-03-09 12:41:52 -0500

    Fixed again again???

--- MovingPlatforms.ts
+++ MovingPlatforms.ts
@@ -4,12 +4,8 @@
 
 //% color=#42f551 icon="↔"
 namespace pathfinder {
-    //% block="AutoCreateMovingPlatforms. Platform tile $platform_image, Start tile $start_image, End tile $end_image, Background tile $background_image."
+    //% block="AutoCreateMovingPlatforms. Platform tile $platform_image=tileset_tile_picker=tileset_tile_picker, Start tile $start_image=tileset_tile_picker, End tile $end_image, Background tile $background_image=tileset_tile_picker."
     //% inlineInputMode=inline
-    //% platform_image=tileset_tile_picker
-    //% start_image=tileset_tile_picker
-    //% end_image=tileset_tile_picker
-    //% background_image=tileset_tile_picker
     export function createPlatforms(platform_image: Image, start_image: Image, end_image: Image, background_image: Image): void {
         MovingPlatform.fromCurrentTilemap(platform_image, start_image, end_image, background_image);
     }
