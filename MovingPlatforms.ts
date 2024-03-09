66b39d693521bfb7062d5cb9a711b3d816ea3a67
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-03-09 12:19:46 -0500

    Dropdowns

--- MovingPlatforms.ts
+++ MovingPlatforms.ts
@@ -4,12 +4,12 @@
 
 //% color=#42f551 icon="↔"
 namespace pathfinder {
-    //% block="AutoCreateMovingPlatforms $platform_image $start_image $end_image $background_image"
+    //% block="AutoCreateMovingPlatforms. Platform tile $platform_image, Start tile $start_image, End tile $end_image, Background tile $background_image."
     //% inlineInputMode=inline
-    //% platform_image.defl="tileset_tile_picker"
-    //% start_image.defl="tileset_tile_picker"
-    //% end_image.defl="tileset_tile_picker"
-    //% background_image.defl="tileset_tile_picker"
+    //% platform_image.shadow="tileset_tile_picker"
+    //% start_image.shadow="tileset_tile_picker"
+    //% end_image.shadow="tileset_tile_picker"
+    //% background_image.shadow="tileset_tile_picker"
     export function createPlatforms(platform_image: Image, start_image: Image, end_image: Image, background_image: Image): void {
         MovingPlatform.fromCurrentTilemap(platform_image, start_image, end_image, background_image);
     }
