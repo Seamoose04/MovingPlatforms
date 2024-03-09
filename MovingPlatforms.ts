550e44edae6139e1db0b4409b604fa2d56aab4fd
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-03-09 12:01:44 -0500

    Added variable pickers

--- MovingPlatforms.ts
+++ MovingPlatforms.ts
@@ -4,7 +4,12 @@
 
 //% color=#42f551 icon="↔"
 namespace pathfinder {
-    //% block
+    //% block="AutoCreateMovingPlatforms $platform_image $start_image $end_image $background_image"
+    //% inlineInputMode=inline
+    //% platform_image.defl="tile_image_picker"
+    //% start_image.defl="tile_image_picker"
+    //% end_image.defl="tile_image_picker"
+    //% background_image.defl="tile_image_picker"
     export function createPlatforms(platform_image: Image, start_image: Image, end_image: Image, background_image: Image): void {
         MovingPlatform.fromCurrentTilemap(platform_image, start_image, end_image, background_image);
     }
