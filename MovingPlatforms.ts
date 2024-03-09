2493260e1f76d6db6ce04a09cb7bc06075dbfea2
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-03-09 12:16:46 -0500

    Fixed tilepicker

--- MovingPlatforms.ts
+++ MovingPlatforms.ts
@@ -6,10 +6,10 @@
 namespace pathfinder {
     //% block="AutoCreateMovingPlatforms $platform_image $start_image $end_image $background_image"
     //% inlineInputMode=inline
-    //% platform_image.defl="tile_image_picker"
-    //% start_image.defl="tile_image_picker"
-    //% end_image.defl="tile_image_picker"
-    //% background_image.defl="tile_image_picker"
+    //% platform_image.defl="tileset_tile_picker"
+    //% start_image.defl="tileset_tile_picker"
+    //% end_image.defl="tileset_tile_picker"
+    //% background_image.defl="tileset_tile_picker"
     export function createPlatforms(platform_image: Image, start_image: Image, end_image: Image, background_image: Image): void {
         MovingPlatform.fromCurrentTilemap(platform_image, start_image, end_image, background_image);
     }
