9b8acc5d018e13aa22ce006d2282eaf87c8d6e50
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-03-09 12:37:18 -0500

    Maybe?

--- MovingPlatforms.ts
+++ MovingPlatforms.ts
@@ -6,10 +6,10 @@
 namespace pathfinder {
     //% block="AutoCreateMovingPlatforms. Platform tile $platform_image, Start tile $start_image, End tile $end_image, Background tile $background_image."
     //% inlineInputMode=inline
-    //% platform_image.def="tileset_tile_picker"
-    //% start_image.def="tileset_tile_picker"
-    //% end_image.def="tileset_tile_picker"
-    //% background_image.def="tileset_tile_picker"
+    //% platform_image="tileset_tile_picker"
+    //% start_image="tileset_tile_picker"
+    //% end_image="tileset_tile_picker"
+    //% background_image="tileset_tile_picker"
     export function createPlatforms(platform_image: Image, start_image: Image, end_image: Image, background_image: Image): void {
         MovingPlatform.fromCurrentTilemap(platform_image, start_image, end_image, background_image);
     }
