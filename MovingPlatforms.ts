d76a066411bfd68b826f6fba8b428f3dc1706011
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-03-12 16:10:45 -0400

    Fixed Block

--- MovingPlatforms.ts
+++ MovingPlatforms.ts
@@ -4,7 +4,7 @@
 
 //% color=#42f551 icon="↔"
 namespace pathfinder {
-    //% block="AutoCreateMovingPlatforms. Platform tile %platform_image=tileset_tile_picker=tileset_tile_picker, Start tile %start_image=tileset_tile_picker, End tile %end_image=tileset_tile_picker, Background tile %background_image=tileset_tile_picker."
+    //% block="AutoCreateMovingPlatforms. Platform tile %platform_image=tileset_tile_picker, Start tile %start_image=tileset_tile_picker, End tile %end_image=tileset_tile_picker, Background tile %background_image=tileset_tile_picker."
     //% inlineInputMode=inline
     export function createPlatforms(platform_image: Image, start_image: Image, end_image: Image, background_image: Image): void {
         MovingPlatform.fromCurrentTilemap(platform_image, start_image, end_image, background_image);
