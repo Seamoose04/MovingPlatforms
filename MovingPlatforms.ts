e56a5e16ea32b5159b0cca2da74d7979e6737308
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-03-14 18:22:50 -0400

    Update MovingPlatforms.ts

--- MovingPlatforms.ts
+++ MovingPlatforms.ts
@@ -4,10 +4,10 @@
 
 //% color=#42f551 icon="↔"
 namespace Platforms {
-    //% block="AutoCreateMovingPlatforms. Platform tile %platform_image=tileset_tile_picker, Start tile %start_image=tileset_tile_picker, End tile %end_image=tileset_tile_picker, Background tile %background_image=tileset_tile_picker."
+    //% block="AutoCreateMovingPlatforms. Platform tile %platform_image=tileset_tile_picker, Background tile %background_image=tileset_tile_picker, Start tile %start_image=tileset_tile_picker, End tile %end_image=tileset_tile_picker."
     //% inlineInputMode=inline
     export function createMovingPlatforms(platform_image: Image, start_image: Image, end_image: Image, background_image: Image): void {
-        MovingPlatform.fromScene(platform_image, start_image, end_image, background_image);
+        MovingPlatform.fromScene(platform_image, background_image, start_image, end_image);
     }
 
     // export function generatePlatformWalls(platform_image: Image) {
